8de548676ff5545921acefa578d2ef78c01503a8
Dora Belme <contact.dorabelme@gmail.com>
2019-09-06 11:43:25 -0400

    set up insomnia and get request

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const actionsDb = require("../data/helpers/actionModel.js");
+const actionDb = require("../data/helpers/actionModel.js");
 
 const router = express.Router();
 
+// GET requests for actions
+router.get('/', (req, res) => {
+    actionDb.get()
+        .then(actions => res.status(200).json(actions))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The actions information could not be retrieved." });
+        });
+});
 
 module.exports = router;
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -3,5 +3,15 @@ const projectDb = require("../data/helpers/projectModel.js");
 
 const router = express.Router();
 
+// GET requests for projects
+router.get('/', (req, res) => {
+    projectDb.get()
+        .then(projects => res.status(200).json(projects))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The projects information could not be retrieved." });
+        });
+});
+
 
 module.exports = router;
