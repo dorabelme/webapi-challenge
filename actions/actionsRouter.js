3b0972132df13ecc07d14d9b848254cfb93e6c6d
Dora Belme <contact.dorabelme@gmail.com>
2019-09-06 12:10:34 -0400

    set up post and delete requests and tested with insomnia

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -32,6 +32,57 @@ router.get('/:id', (req, res) => {
     //     });
 });
 
+
+// POST requests for actions
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const { project_id, description, notes } = req.body;
+    if (!project_id || !description || !notes) {
+        return res.status(400).json({ error: "Requires project_id, description, and notes." });
+    }
+    actionDb.insert({ project_id, description, notes })
+        .then(({ id }) => {
+            getAction(id, res)
+            // actionDb.get(id)
+            //     .then(action => {
+            //         console.log(action);
+            //         if (action) {
+            //             res.status(201).json(action);
+            //         } else {
+            //             res.status(404).json({ error: "Action with ID does not exist." })
+            //         }
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //         res.status(500).json({ error: "Server error retrieving action." });
+            //     });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Server error inserting action." });
+        });
+});
+
+
+// DELETE request for actions
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    actionDb.remove(id)
+        .then(deleted => {
+            console.log(deleted);
+            if (deleted) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ error: "Project with ID does not exist." });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Server error deleting project." });
+        });
+});
+
+
 function getAction(id, res) {
     return actionDb.get(id)
         .then((action) => {
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -31,6 +31,56 @@ router.get('/:id', (req, res) => {
     //     });
 });
 
+
+// POST request for projects
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const { name, description } = req.body;
+    if (!name || !description) {
+        return res.status(400).json({ error: "Requires name and description." });
+    }
+    projectDb.insert({ name, description })
+        .then(({ id }) => {
+            getProject(id, res)
+            // projectDb.get(id)
+            //     .then(project => {
+            //         console.log(project);
+            //         if (project) {
+            //             res.status(201).json(project);
+            //         } else {
+            //             res.status(404).json({error: "Project with ID does not exist."})
+            //         }
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //         res.status(500).json({ error: "Server error retrieving project."});
+            //     });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Server error inserting project." });
+        });
+});
+
+
+// DELETE request for projects
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    projectDb.remove(id)
+        .then(deleted => {
+            console.log(deleted);
+            if (deleted) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ error: "Project with ID does not exist." });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Server error deleting project." });
+        });
+});
+
 function getProject(id, res) {
     return projectDb.get(id)
         .then((project) => {
